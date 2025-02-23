2c81dd146f9665912b18f59310dd86cec69685dd
Ámán Gergely <amangergely@gmail.com>
2025-02-23 12:28:11 +0100

    Add project to repo.

---
+++ PleaseDoTutorial/PleaseDoTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PleaseDoTutorial
+//
+//  Created by Ámán Gergely on 2025. 02. 23..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PleaseDoTutorial/PleaseDoTutorial/PleaseDoTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseDoTutorialApp.swift
+//  PleaseDoTutorial
+//
+//  Created by Ámán Gergely on 2025. 02. 23..
+//
+
+import SwiftUI
+
+@main
+struct PleaseDoTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
